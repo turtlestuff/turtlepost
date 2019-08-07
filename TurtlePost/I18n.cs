18080a0bd6b806547915c87c34787e1de1fd9eba
reflectronic <john-tur@outlook.com>
2019-08-07 10:49:38 -0400

    Fix formatting

--- TurtlePost/I18n.cs
+++ TurtlePost/I18n.cs
@@ -1,43 +1,42 @@
-#pragma warning disable 169
-
-using System;
 using System.Globalization;
 using System.Collections.Generic;
 using System.Reflection;
-using System.IO;
+using System.Linq;
 using System.Text.Json;
 
 namespace TurtlePost
 {
     public static class I18n
-    {        
-        private static Dictionary<string, string> strings;
+    {
+        static Dictionary<string, string> strings = new Dictionary<string, string>();
         
         static I18n()
         {
-            strings = new Dictionary<string, string>();
-            
             InsertResources($"TurtlePost.Localization.{CultureInfo.CurrentUICulture.IetfLanguageTag}.json");
             InsertResources("TurtlePost.Localization.en-US.json");
         }
-        
-        private static void InsertResources(string manifestResourceName) {
-            Stream? resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(manifestResourceName);
+
+        static void InsertResources(string manifestResourceName) 
+        {
+            var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(manifestResourceName);
             
-            if (resourceStream == null) {
+            if (resourceStream == null) 
+            {
                 //Do nothing; this language is not found.
-            } else {
-                JsonElement englishStrings = JsonDocument.Parse(resourceStream).RootElement;
-                foreach (JsonProperty prop in englishStrings.EnumerateObject()) {
-                    if (!strings.ContainsKey(prop.Name)) strings.Add(prop.Name, prop.Value.GetString());
+            } 
+            else
+            {
+                var englishStrings = JsonDocument.Parse(resourceStream).RootElement;
+                foreach (var prop in englishStrings.EnumerateObject().Where(prop => !strings.ContainsKey(prop.Name)))
+                {
+                    strings.Add(prop.Name, prop.Value.GetString());
                 }
             }
         }
         
         public static string _(string key)
         {
-            if (strings.ContainsKey(key)) return strings[key];
-            return key;
+            return strings.ContainsKey(key) ? strings[key] : key;
         }
     }
 }
--- TurtlePost/Program.cs
+++ TurtlePost/Program.cs
@@ -1,103 +1,129 @@
 using System;
-using System.Linq.Expressions;
 using System.Reflection;
-using System.Reflection.Emit;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
+using System.Text;
 using static TurtlePost.I18n;
 
 namespace TurtlePost
 {
     static class Program
     {
-        struct KeyPressed {
+        struct KeyPressed
+        {
             ConsoleKey key;
             ConsoleModifiers modifiers;
-            
-            public KeyPressed(ConsoleKey key, ConsoleModifiers modifiers) {
+
+            public KeyPressed(ConsoleKey key, ConsoleModifiers modifiers)
+            {
                 this.key = key;
                 this.modifiers = modifiers;
             }
         }
-        
-        private static Interpreter DirectInterpreter;
-        private static bool InputComplete = false;
-        private static string CurrentInput = "";
-        private static Dictionary<KeyPressed, Action> Keys = new Dictionary<KeyPressed, Action> {
-            {new KeyPressed(ConsoleKey.D, ConsoleModifiers.Control), () => {
-                if (CurrentInput == "") {
-                    Console.WriteLine("exit");
-                    DirectInterpreter.Interpret("exit", true);
+
+        static Interpreter DirectInterpreter;
+        static bool InputComplete = false;
+        static StringBuilder CurrentInput = new StringBuilder(256);
+
+        static Dictionary<KeyPressed, Action> Keys = new Dictionary<KeyPressed, Action>
+        {
+            {
+                new KeyPressed(ConsoleKey.D, ConsoleModifiers.Control), () =>
+                {
+                    if (CurrentInput.Length == 0)
+                    {
+                        Console.WriteLine("exit");
+                        DirectInterpreter.Interpret("exit", true);
+                        InputComplete = true;
+                    }
+                    else
+                    {
+                        //There is currently input
+                        Console.Beep();
+                    }
+                }
+            },
+            {
+                new KeyPressed(ConsoleKey.Enter, 0), () =>
+                {
+                    //Interpret the current line
+                    Console.WriteLine();
+                    DirectInterpreter.Interpret(CurrentInput.ToString(), true);
                     InputComplete = true;
-                } else {
-                    //There is currently input
-                    Console.Beep();
                 }
-            }},
-            {new KeyPressed(ConsoleKey.Enter, 0), () => {
-                //Interpret the current line
-                Console.WriteLine();
-                DirectInterpreter.Interpret(CurrentInput, true);
-                InputComplete = true;
-            }},
-            {new KeyPressed(ConsoleKey.Backspace, 0), () => {
-                if (CurrentInput == "") {
-                    //Nothing has been typed
-                    Console.Beep();
-                } else {
-                    //Back up the caret and replace it with a space
-                    Console.Write("\b \b");
-                    CurrentInput = CurrentInput[..^1].ToString();
+            },
+            {
+                new KeyPressed(ConsoleKey.Backspace, 0), () =>
+                {
+                    if (CurrentInput.Length == 0)
+                    {
+                        //Nothing has been typed
+                        Console.Beep();
+                    }
+                    else
+                    {
+                        //Back up the caret and replace it with a space
+                        Console.Write("\b \b");
+                        CurrentInput = CurrentInput.Remove(CurrentInput.Length - 1, 1);
+                    }
                 }
-            }},
-            {new KeyPressed(ConsoleKey.C, ConsoleModifiers.Control), () => {
-                //Reset the input buffer
-                Console.WriteLine();
-                if (CurrentInput == "") {
-                    //Print a message stating how to exit
-                    Console.WriteLine(_("exitExplanation"));
+            },
+            {
+                new KeyPressed(ConsoleKey.C, ConsoleModifiers.Control), () =>
+                {
+                    //Reset the input buffer
+                    Console.WriteLine();
+                    if (CurrentInput.Length == 0)
+                    {
+                        //Print a message stating how to exit
+                        Console.WriteLine(_("exitExplanation"));
+                    }
+
+                    InputComplete = true;
                 }
-                InputComplete = true;
-            }}
+            }
         };
-        
+
         static void Main(string[] args)
         {
             if (args.Length == 0)
             {
-                Console.WriteLine(string.Format(_("versionString"), Assembly.GetExecutingAssembly().GetName().Version));
+                Console.WriteLine(_("versionString"), Assembly.GetExecutingAssembly().GetName().Version);
                 Console.WriteLine("By Vrabbers and Reflectronic, (c) 2019");
                 Console.WriteLine(_("helpPrompt"));
-                
+
                 // Set up the console
                 Console.TreatControlCAsInput = true;
-                
+
                 DirectInterpreter = new Interpreter();
 
                 while (true)
                 {
                     Console.Write("> ");
-                    do {
+                    do
+                    {
                         var key = Console.ReadKey(true);
                         var keyPressed = new KeyPressed(key.Key, key.Modifiers);
-                        if (Keys.ContainsKey(keyPressed)) {
-                            Keys[keyPressed]();
-                        } else if (key.Modifiers == 0) {
+                        if (Keys.TryGetValue(keyPressed, out var func))
+                        {
+                            func();
+                        }
+                        else if (key.Modifiers == 0)
+                        {
                             Console.Write(key.KeyChar);
-                            CurrentInput += key.KeyChar;
+                            CurrentInput.Append(key.KeyChar);
                         }
                     } while (!InputComplete);
-                    
-                    CurrentInput = "";
+
+                    CurrentInput.Clear();
                     InputComplete = false;
                 }
-            } 
+            }
             else
             {
                 // Load file
                 var code = File.ReadAllText(args[0]);
-                new Interpreter().Interpret(code,false);
+                new Interpreter().Interpret(code, false);
                 Console.WriteLine();
                 Console.WriteLine(_("exitPrompt"));
                 Console.ReadKey();
